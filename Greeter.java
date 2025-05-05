4d96e97f4c9eb979d955d5222392be0480f909df
JoyJudy Wangui <145750350+JoyJudy@users.noreply.github.com>
2025-05-05 17:07:18 +0300

    Challenge_01

---
+++ Greeter.java
@@ -0,0 +1,15 @@
+package Class_Challenges;
+
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+
+    public static void main(String[] args){
+        (new Greeter()).greet();
+    }
+
+}
+
+
+
